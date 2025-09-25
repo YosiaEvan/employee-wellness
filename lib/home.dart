195841757ce40f964ae1aefff35cdbb824c3a4f3
yosiaevan <yosiaevan23@gmail.com>
2025-09-25 15:51:04 +0700

    membuat header component

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:employee_wellness/header.dart';
 import 'package:employee_wellness/main.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,39 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  Future<void> logout(BuildContext context) async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString("token");
-    try {
-      final response = await http.post(
-        Uri.parse("http://10.0.2.2:8000/api/logout"),
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${token}",
-        }
-      );
-
-      if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Logout berhasil")),
-        );
-
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginPage())
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("Gagal logout")),
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error: $e")),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,110 +17,7 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Header
-            Container(
-              padding: const EdgeInsets.all(16),
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 20,
-                        backgroundImage: AssetImage("assets/img/0L1A6186 - square.JPG"),
-                        backgroundColor: Colors.transparent,
-                      ),
-
-                      const SizedBox(width: 10),
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            "Employee Wellness",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            "Kesehatan & Kebahagiaan Karyawan",
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  Row(
-                    children: [
-                      Stack(
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.notifications_outlined),
-                          ),
-                          Positioned(
-                              right: 8,
-                              top: 8,
-                              child: Container(
-                                width: 8,
-                                height: 8,
-                                decoration: const BoxDecoration(
-                                  color: Colors.red,
-                                  shape: BoxShape.circle,
-                                ),
-                              )
-                          ),
-                        ],
-                      ),
-
-                      const SizedBox(width: 10),
-
-                      PopupMenuButton<String>(
-                        color: Colors.white,
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        onSelected: (value) {
-                          if (value == 'logout') {
-                            logout(context);
-                          }
-                        },
-                        itemBuilder: (BuildContext context) => [
-                          const PopupMenuItem(
-                            value: 'logout',
-                            child: SizedBox(
-                              child: Row(
-                                children: const [
-                                  Icon(Icons.logout, color: Colors.red),
-                                  SizedBox(width: 8),
-                                  Text("Keluar"),
-                                ],
-                              ),
-                            )
-                          ),
-                        ],
-                        child: const CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.grey,
-                          child: Icon(Icons.person, color: Colors.white),
-                        ),
-                      ),
-
-                      // const CircleAvatar(
-                      //   radius: 20,
-                      //   backgroundColor: Colors.grey,
-                      //   child: Icon(Icons.person, color: Colors.white),
-                      // ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            Header(),
 
             // Main Content
             Expanded(
